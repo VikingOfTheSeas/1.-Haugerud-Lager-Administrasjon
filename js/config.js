55794f2ca40e7683de9a903804700c99e2f7e80d
VikingOfTheSeas <159448020+VikingOfTheSeas@users.noreply.github.com>
2026-03-12 19:31:20 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -6,9 +6,9 @@
  
 const SUPABASE_URL     = 'https://mxnojoymdgeapwsljkbg.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im14bm9qb3ltZGdlYXB3c2xqa2JnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMzMTczNzgsImV4cCI6MjA4ODg5MzM3OH0.IZuE0og-G-12Izu2VVyx_kV8sDHUE6nG5Ziucdi-FNk';
- 
-// Basis-URL for QR-lenker — bytt til din GitHub Pages URL
-const APP_BASE_URL = window.location.origin + window.location.pathname.replace('index.html','');
- 
+
+// Denne er allerede ferdig utfylt for deg:
+const APP_BASE_URL = 'https://vikingoftheseas.github.io/speiderlageret/';
+
 const { createClient } = window.supabase;
 const db = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
